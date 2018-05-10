5a8ad75af5e9122288e61ae5c31a1e096111d1b5
baconadmiral <andrewwilliams1087@gmail.com>
2018-05-10 16:13:00 -0600

    Added Cars!

--- customer.js
+++ customer.js
@@ -1,6 +1,14 @@
 
 function Customer(id)
 {
+  this.imageNameList = [];
+  this.imageNameList.push('carRed.png');
+  this.imageNameList.push('carYellow.png');
+  this.imageNameList.push('carGreen.png');
+  this.imageNameList.push('carPurple.png');
+  this.imageNameList.push('carBlue.png');
+
+  this.image = loadImage(random(this.imageNameList));
   this.customerId = id;
   this.waiting = false;
   this.xLoc =  -10;
@@ -14,52 +22,52 @@ function Customer(id)
 
   this.collisionFinished = false;
   this.annealCollide = 20;
-  
+
   this.showCustomer = function()
   {
     fill(this.colorRed, this.colorGreen, this.colorBlue);
-    ellipse(this.xLoc, this.yLoc, 15, 15);
+    image(this.image, this.xLoc-20, this.yLoc);
 
   }
-  
+
   this.update = function(throughPut, customerList)
   {
-    
-    if(this.xLoc >= width / 2 && !this.waited)  
+
+    if(this.xLoc >= width / 2 && !this.waited)
     {
       this.speed = 0;
       this.waiting = true;
-      
+
       if(frameCount % 100)
       {
         this.waitTimer -= throughPut;
       }
-      
+
       if(this.waitTimer <= 0)
       {
         this.waited = true;
         this.waiting = false;
         this.speed = 1.4;
       }
-      
-      
+
+
       this.isCollided(customerList);
-      
+
     }
-    
+
     this.xLoc += this.speed;
   }
-  
+
   this.isCollided = function(customerList)
   {
     for(let i = 0; i < customerList.length-1; i++)
     {
-      if(this.collisionFinished == false && this.customerId != customerList[i].customerId && this.yLoc + 10 >= customerList[i].yLoc && this.yLoc - 10 <= customerList[i].yLoc 
+      if(this.collisionFinished == false && this.customerId != customerList[i].customerId && this.yLoc + 10 >= customerList[i].yLoc && this.yLoc - 10 <= customerList[i].yLoc
         && this.waited == false)
       {
         this.yLoc+=random(-20, 20);
         this.annealCollide--;
-                
+
         if(this.annealCollide < 0)
         {
           this.collisionFinished = true;
@@ -67,5 +75,5 @@ function Customer(id)
       }
     }
   }
-  
+
 }
--- sketch.js
+++ sketch.js
@@ -13,20 +13,20 @@ let customersWaiting;
 let idCtr;
 
 function setup() {
-  
+
   idCtr = 0;
   divider = new Divider();
-  
+
   customerList = [];
   customersWaiting = 0;
-  
+
   canvas = createCanvas(500, 400);
   bgcolor = 200;
-  createP(" ");  
-  
+  createP(" ");
+
   arrivalRateSlider = select("#arrival");
   throughputSlider = select("#throughPut");
-  
+
 }
 
 function changeColor()
@@ -43,21 +43,21 @@ function addCustomer()
 
 function draw() {
   background(bgcolor);
-  
+
   divider.showDivider();
   text("Arrival Rate: " + arrivalRateSlider.value(), 10, 20);
   text("Throughput: " + throughputSlider.value(), 10, 50);
-  text("Number of Customers: " + arrivalRateSlider.value() * throughputSlider.value(), 10, 80);
-  text("Customers Waiting: " + customersWaiting, 10, 110);
-  
+  text("Number of Cars: " + arrivalRateSlider.value() * throughputSlider.value(), 10, 80);
+  text("Cars Waiting: " + customersWaiting, 10, 110);
+
 
   if(frameCount % (60 - arrivalRateSlider.value()) == 0)
   {
     addCustomer();
   }
-    
-  
-  
+
+
+
   let howManyWaitingThisCycle = 0;
   for(var i = 0; i < customerList.length-1; i++)
   {
@@ -68,7 +68,7 @@ function draw() {
     }
     customerList[i].showCustomer();
   }
-  
+
   customersWaiting = howManyWaitingThisCycle;
-  
+
 }
