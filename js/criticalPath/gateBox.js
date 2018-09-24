8dea64818949576a0bfc3cfe7a7160218a4fc47f
baconadmiral <andrewwilliams1087@gmail.com>
2018-09-24 15:24:04 -0600

    flow counter

--- js/criticalPath/gateBox.js
+++ js/criticalPath/gateBox.js
@@ -1,5 +1,6 @@
 function GateBox(sketch, posX, posY, description, flow, afterBoxList)
 {
+  this.clicked = false;
   this.posX = posX;
   this.posY = posY;
 
@@ -16,6 +17,7 @@ function GateBox(sketch, posX, posY, description, flow, afterBoxList)
 
   this.update = function()
   {
+    sketch.textSize(12);
     sketch.fill(0);
     sketch.text(this.description, this.posX + 5, this.posY + this.h/3);
     sketch.text("Flow: " + this.flow, this.posX + 5, this.posY + this.h/2 + this.h/4);
@@ -43,8 +45,19 @@ function GateBox(sketch, posX, posY, description, flow, afterBoxList)
     sketch.line(this.posX+this.w/2, this.posY+this.h, afterBox.posX+this.w/2, afterBox.posY);
   }
 
-  this.clickSuccess = function()
+  this.click = function()
   {
-    this.g = 255;
+    this.r = 204;
+    this.g = 102;
+    this.b = 0;
+    this.clicked = true;
+  }
+
+  this.unclick = function()
+  {
+    this.r = 10;
+    this.g = 10;
+    this.b = 10;
+    this.clicked = false;
   }
 }
--- js/criticalPath/sketch.js
+++ js/criticalPath/sketch.js
@@ -3,6 +3,7 @@ var cPathSim = function(sketch) {
   var defaultBoxSpacing = 90;
   var defaultBoxSpacingWidth = 120;
   var defaultWidth = 60;
+  var flowTotal = 0;
 
   sketch.setup = function() {
     sketch.frameRate(30);
@@ -16,13 +17,13 @@ var cPathSim = function(sketch) {
     var sendBox = new GateBox(sketch, centerBox, 400, "Send", 0);
     gateBoxList.push(sendBox);
 
-    var paintBox = new GateBox(sketch, centerBox, sendBox.posY - defaultBoxSpacing, "Paint", 40, sendBox);
+    var paintBox = new GateBox(sketch, centerBox, sendBox.posY - defaultBoxSpacing, "Paint", 5, sendBox);
     gateBoxList.push(paintBox);
 
-    var sandingBox = new GateBox(sketch, centerBox,paintBox.posY - defaultBoxSpacing, "Sand", 40, paintBox);
+    var sandingBox = new GateBox(sketch, centerBox,paintBox.posY - defaultBoxSpacing, "Sand", 18, paintBox);
     gateBoxList.push(sandingBox);
 
-    var washBox = new GateBox(sketch, centerBox, sandingBox.posY - defaultBoxSpacing, "Wash", 40, sandingBox);
+    var washBox = new GateBox(sketch, centerBox, sandingBox.posY - defaultBoxSpacing, "Wash", 12, sandingBox);
     gateBoxList.push(washBox);
 
     var rootPathList = [];
@@ -31,21 +32,22 @@ var cPathSim = function(sketch) {
     //Not critical path boxes
     var hammeringBox = new GateBox(sketch, centerBox-defaultBoxSpacingWidth, paintBox.posY - defaultBoxSpacing, "Nailing", 5, sendBox);
     gateBoxList.push(hammeringBox);
-    var flatteningBox = new GateBox(sketch, centerBox-defaultBoxSpacingWidth, hammeringBox.posY - defaultBoxSpacing, "Flattening", 5, hammeringBox);
+    var flatteningBox = new GateBox(sketch, centerBox-defaultBoxSpacingWidth, hammeringBox.posY - defaultBoxSpacing, "Flattening", 20, hammeringBox);
     gateBoxList.push(flatteningBox);
 
     rootPathList.push(flatteningBox);
 
-    var rivetingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, sendBox.posY - defaultBoxSpacing, "Welding", 5, sendBox);
+    //Critical Path Boxes
+    var rivetingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, sendBox.posY - defaultBoxSpacing, "Welding", 18, sendBox);
     gateBoxList.push(rivetingBox);
-    var weldingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, rivetingBox.posY - defaultBoxSpacing, "Riveting", 5, rivetingBox);
+    var weldingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, rivetingBox.posY - defaultBoxSpacing, "Riveting", 20, rivetingBox);
     gateBoxList.push(weldingBox);
-    var gluingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, weldingBox.posY - defaultBoxSpacing, "Gluing", 5, weldingBox);
+    var gluingBox = new GateBox(sketch, centerBox+defaultBoxSpacingWidth, weldingBox.posY - defaultBoxSpacing, "Gluing", 19, weldingBox);
     gateBoxList.push(gluingBox);
 
     rootPathList.push(gluingBox);
 
-    var receiveBox = new GateBox(sketch, centerBox, washBox.posY - defaultBoxSpacing, "Receive", 40, rootPathList);
+    var receiveBox = new GateBox(sketch, centerBox, washBox.posY - defaultBoxSpacing, "Receive", 5, rootPathList);
     gateBoxList.push(receiveBox);
 
 
@@ -59,15 +61,28 @@ var cPathSim = function(sketch) {
       gateBoxList[i].update();
     }
 
+    sketch.fill(0);
+    sketch.textSize(20);
+    sketch.text("Selected Flow: " + flowTotal, 130, 30);
+
   }
 
-  sketch.mousePressed = function()
+  sketch.mouseClicked = function()
   {
     for(let i = 0; i < gateBoxList.length; i++)
     {
       if(sketch.mouseX >= gateBoxList[i].posX && sketch.mouseX < gateBoxList[i].posX+gateBoxList[i].w && sketch.mouseY >= gateBoxList[i].posY && sketch.mouseY < gateBoxList[i].posY+gateBoxList[i].h)
       {
-        gateBoxList[i].clickSuccess();
+        if(!gateBoxList[i].clicked)
+        {
+          flowTotal += gateBoxList[i].flow;
+          gateBoxList[i].click();
+        }
+        else {
+          flowTotal -= gateBoxList[i].flow;
+          gateBoxList[i].unclick();
+        }
+
       }
     }
   }
