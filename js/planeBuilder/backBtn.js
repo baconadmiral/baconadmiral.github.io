aa8f524b38bf3296763da5fdaf16705756974318
baconadmiral <andrewwilliams1087@gmail.com>
2019-01-24 15:45:52 -0700

    added missing files

---
+++ js/planeBuilder/backBtn.js
@@ -0,0 +1,20 @@
+function BackBtn(sketch, posX, posY, backBtnImg)
+{
+  this.xPos = posX;
+  this.yPos = posY;
+
+  this.update = function()
+  {
+    sketch.image(backBtnImg, this.xPos, this.yPos, 35, 35);
+  }
+
+  this.mousePressed = function()
+  {
+    if(sketch.dist(sketch.mouseX, sketch.mouseY, this.xPos, this.yPos) < 35)
+    {
+      return true;
+    }
+
+    return false;
+  }
+}
