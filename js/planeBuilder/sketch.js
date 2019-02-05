b4159ac3bff7ca58ad539351e7aac43d0104ba26
jenkins <jenkins@jslv4-prod-gw1a.aws.di2e.net>
2019-02-05 21:32:14 +0000

    Deploying Application

--- js/planeBuilder/sketch.js
+++ js/planeBuilder/sketch.js
@@ -102,10 +102,10 @@ var planeBuilderSim = function(sketch) {
 
     //can = sketch.createCanvas(canvasWidth, canvasHeight);
     //console.log(sketch.windowHeight);
-    can = sketch.createCanvas(700, 350);
-    sketch.frameRate(40);
-    const canvasElt = can.elt;
-    canvasElt.style.width = '100%'; canvasElt.style.height = "100%";
+    //can = sketch.createCanvas(700, 350);
+    //sketch.frameRate(40);
+    //const canvasElt = can.elt;
+    //canvasElt.style.width = '100%'; canvasElt.style.height = "100%";
 
     let defaultToolsLocationX = sketch.width/2 -32;
     let defaultToolsLocationY = sketch.height - sketch.height/6;
